a975c7416b8ef0245d31217b7ca32cf95c8630e7
Wilson15112011 <wilsonmichael15112011@gmail.com>
2026-09-12 13:35:38 +0300

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -78,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>نسيت كلمة المرور – كنيسة العذراء والملاك ميخائيل</title>
+    <link rel="icon" type="image/png" href="logo.png">
     <link rel="stylesheet" href="login.css">
     <style>
         .forgot-container {
